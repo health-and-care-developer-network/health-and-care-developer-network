295f71c088c443881b1332e51b67853c9867bbc7
Health and Care Developer Network (Raphael Cohn) <health-and-care-developer-network@stormmq.com>
2013-03-21 14:34:46 +0000

    ipset package

--- source/build/build/BuildScript.java
+++ source/build/build/BuildScript.java
@@ -184,6 +184,8 @@ public final class BuildScript extends AbstractIntelliJConvenientBuildScript
 
 		debianPackagesPackageTask("hdn-nss", "generate changelog template");
 
+		debianPackagesPackageTask("hdn-ipset", "generate changelog template");
+
 		task("packages").dependsOn(debianPackagesPackageTasks).does
 		(
 			execute(source("build", "build").file("create-insecure-apt-repository")).inWorkingDirectory(output()).forUpTo(OneHour).withInheritedEnvironmentVariables().withArguments("packages"),
